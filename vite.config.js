f73c877f9c5d81ef15786ffdfedb61fb4f58add3
rifqirm <rifqirm15122001@gmail.com>
2023-05-12 19:31:59 +0700

    push coba deploy yg error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  global: {},
+  build: {      
+    rollupOptions: {
+        external: '@fortawesome/fontawesome-svg-core'
+      }
+    }
 })
+
