0bb91a93617b3e75193d1c5ee2f7ff2c37cb6b91
rifqirm <125572655+rifqirm@users.noreply.github.com>
2024-04-29 10:17:40 +0700

    Update CreateProduct.jsx

--- src/pages/CreateProduct/CreateProduct.jsx
+++ src/pages/CreateProduct/CreateProduct.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from '../../components/Header/Header';
 import Form from '../../components/Form/Form';
-import Table from '../../components/Table/Table';
+// import Table from '../../components/Table/Table';
 // import { useContext, useState } from 'react';
 // import ProductsContext from "../../context/ProductsContext";
 
@@ -13,7 +13,7 @@ function CreateProduct() {
     <>
       <Header />
       <Form/>
-      <Table />
+{/*       <Table /> */}
       
     </>
       
